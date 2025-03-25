10e6b8f574b0ad27fe57669feece109b2cd85cee
Felix Schreiber <felix.schreiber@auctores.de>
2025-03-25 16:40:11 +0100

    added image support to markdown editor

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,5 +1,5 @@
 import {Main} from "@/component/container/container";
-import MDEditor from "@/component/md-editor/md-editor";
+import MDEditor, {Data} from "@/component/md-editor/md-editor";
 import {Metadata} from "next";
 
 export const metadata: Metadata = {
@@ -7,11 +7,19 @@ export const metadata: Metadata = {
     description: "Die Markdown-Editor-Seite",
 };
 
+async function submit(data: Data): Promise<{ success: boolean, message?: string }> {
+    "use server";
+
+    console.log(data);
+
+    return {success: true};
+}
+
 export default async function Page() {
     return (
         <Main>
             <h1>Markdown Editor</h1>
-            <MDEditor/>
+            <MDEditor onSubmit={submit}/>
         </Main>
     );
 }
--- src/component/container/container.tsx
+++ src/component/container/container.tsx
@@ -5,27 +5,31 @@ import styles from "./container.module.scss";
 type Props<E extends ElementType> = {
     as: E,
     slim?: boolean;
+    full?: boolean;
 } & ComponentPropsWithRef<E>
 
 export default async function Container<E extends ElementType>(
     {
         as: Component,
         slim,
+        full,
         className,
         ...props
     }: Props<E>,
 ) {
-    return <Component className={`${styles.container} ${slim ? styles.slim : ""} ${className ?? ""}`} {...props}/>;
+    return <Component
+        className={`${styles.container} ${slim ? styles.slim : ""} ${full ? styles.full : ""} ${className ?? ""}`}
+        {...props}/>;
 }
 
 export const Main = async (
-    {slim, ...props}: Omit<Props<"main">, "as">,
-) => <Container<"main"> as="main" slim={slim} {...props}/>;
+    props: Omit<Props<"main">, "as">,
+) => <Container<"main"> as="main" {...props}/>;
 
 export const Section = async (
-    {slim, ...props}: Omit<Props<"section">, "as">,
-) => <Container<"section"> as="section" slim={slim} {...props}/>;
+    props: Omit<Props<"section">, "as">,
+) => <Container<"section"> as="section" {...props}/>;
 
 export const Div = async (
-    {slim, ...props}: Omit<Props<"div">, "as">,
-) => <Container<"div"> as="div" slim={slim} {...props}/>;
+    props: Omit<Props<"div">, "as">,
+) => <Container<"div"> as="div" {...props}/>;
