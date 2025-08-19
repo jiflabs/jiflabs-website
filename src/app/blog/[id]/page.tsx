402eb0191ca970d2cd4a7c82245a773e36f8c78f
Felix Schreiber <felix.schreiber@auctores.de>
2025-08-19 09:42:45 +0200

    stronger contrast for button / link borders, style transitions, sub-comment extension / collapse

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,14 +1,12 @@
+import {CommentTree} from "@/component/comment-tree/comment-tree";
 import {Main} from "@/component/container/container";
 import {getArticleResolvedById} from "@/content/article";
 import {Comment} from "@/content/comment";
-import {getUserById} from "@/content/user";
+import {getUserById, User} from "@/content/user";
 import {formatDate} from "@/util/date";
-import {tree, TreeNode} from "@/util/tree";
-import {faThumbsUp} from "@fortawesome/free-regular-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {tree} from "@/util/tree";
 import {UUID} from "crypto";
 import {Metadata} from "next";
-import styles from "./page.module.scss";
 
 type Props = {
     params: Promise<{ id: UUID }>
@@ -25,42 +23,15 @@ export async function generateMetadata({params}: Props): Promise<Metadata> {
     };
 }
 
-type CommentTreeLevelProps = {
-    tree: TreeNode<Comment>[],
-};
-
-function CommentTreeLevel({tree}: Readonly<CommentTreeLevelProps>) {
-    if (!tree.length)
-        return;
-
-    return (
-        <ul className={styles.comment_tree}>
-            {tree.map(async ({value, children}) => {
-                const user = await getUserById(value.user_id);
-
-                return (
-                    <li key={value.id}>
-                        <div className={styles.comment_item}>
-                            <span className={styles.user_name}>{user.name}</span>
-                            <time dateTime={new Date(value.create_date).toISOString()}>
-                                {formatDate(value.create_date)}
-                            </time>
-                            <span>{value.content}</span>
-                            <span><FontAwesomeIcon icon={faThumbsUp}/> {value.likes}</span>
-                        </div>
-                        <CommentTreeLevel tree={children}/>
-                    </li>
-                );
-            })}
-        </ul>
-    );
-}
-
 export default async function Page({params}: Readonly<Props>) {
     const {id} = await params;
     const {title, subtitle, create_date, content, _users, _comments} = await getArticleResolvedById(id);
 
-    const comment_tree = tree(_comments, "comment_id");
+    const comments: (Comment & { _user: User })[] = await Promise.all(_comments.map(async comment => ({
+        ...comment,
+        _user: await getUserById(comment.user_id),
+    })));
+    const comment_tree = tree(comments, "comment_id");
 
     return (
         <Main>
@@ -68,17 +39,17 @@ export default async function Page({params}: Readonly<Props>) {
                 <h1>{title}</h1>
                 <h2>{subtitle}</h2>
                 <p>
-                    am&nbsp;
+                    <span>am </span>
                     <time dateTime={new Date(create_date).toISOString()}>
                         {formatDate(create_date)}
                     </time>
-                    , von {_users.map(user => user.name).join(", ")}
+                    <span>, von {_users.map(user => user.name).join(", ")}</span>
                 </p>
             </div>
             {content}
             <hr/>
             <h2>Kommentare</h2>
-            <CommentTreeLevel tree={comment_tree}/>
+            <CommentTree tree={comment_tree}/>
         </Main>
     );
 }
